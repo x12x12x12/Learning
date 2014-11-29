326803178c656a8686af4a87aa4e584ac730766d
Đỗ Minh Quân <dominhquan@users.noreply.github.com>
2014-11-29 11:17:45 +0700

    Update ItemServiceImpl.java

--- Spring Web MVC/SpringRest_ForAndroidApp/src/main/java/com/dominhquan/service/ItemServiceImpl.java
+++ Spring Web MVC/SpringRest_ForAndroidApp/src/main/java/com/dominhquan/service/ItemServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
 import com.dominhquan.model.Item;
+import com.mongodb.MongoException;
 
 public class ItemServiceImpl implements ItemService{
 	
@@ -24,7 +25,7 @@ public class ItemServiceImpl implements ItemService{
 		this.mongoTemplate=mongoTemplate;
 	}
 	@Override
-	public void createItem(Item item) {
+	public void createItem(Item item) throws MongoException{
 		if(!mongoTemplate.collectionExists(Item.class)){
 			mongoTemplate.createCollection(Item.class);
 		}
@@ -33,31 +34,42 @@ public class ItemServiceImpl implements ItemService{
 	}
 
 	@Override
-	public Item getItem(String id) {
+	public Item getItem(String id) throws MongoException{
 		query=new Query(Criteria.where("_id").is(id));
 		return mongoTemplate.findOne(query, Item.class);
 	}
 	
 	@Override
-	public void removeItem(int id) {
-		// TODO Auto-generated method stub
+	public Item removeItem(String id) throws MongoException{
+		query=new Query(Criteria.where("_id").is(id));
+		Item item=mongoTemplate.findAndRemove(query, Item.class);
 		
 	}
 
 	@Override
-	public void updateItem(Item item) {
+	public void updateItem(Item item) throws MongoException{
 		mongoTemplate.save(item);
 		logger.info("Save item successfully, Details : "+ item.toString());
 	}
 	@Override
-	public List<Item> getListItem(String restaurant) {
+	public List<Item> getListItem(String restaurant) throws MongoException{
 		query=new Query(Criteria.where("restaurant_name").regex(restaurant));
 		return mongoTemplate.find(query, Item.class);
 	}
 	
-	public List<Item> getListHotItem(String restaurant){
+	public List<Item> getListHotItem(String restaurant) throws MongoException{
 		query=new Query(Criteria.where("restaurant_name").is(restaurant).and("status").is(1));
 		query.with(new Sort(Sort.Direction.DESC,"updateDate"));
 		return mongoTemplate.find(query, Item.class);
 	}
+	
+	public int countItem() throws MongoException{
+//		query=new Query(Criteria.where("_id").)
+	}
 }
+
+
+
+
+
+
