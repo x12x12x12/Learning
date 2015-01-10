0455cc11a7e333d753f6835828c3f5d94e132d17
dominhquan <dominhquan.uit@gmail.com>
2015-01-10 16:55:29 +0700

    Core change

--- Spring Web MVC/ChineeseChessOnlie/CoTuongOnline/src/main/java/com/cotuong/uri/AppRestUri.java
+++
@@ -1,8 +0,0 @@
-package com.cotuong.uri;
-
-public class AppRestUri {
-	public static final String get_item="/rest/account/{id}";
-	public static final String check_account="rest/check";
-	public static final String active_account="rest/active";
-	public static final String player_Online="rest/online";
-}
