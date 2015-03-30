8ecf6e9215d2b250ead271cd99afe1cf51a406e7
dominhquan <dominhquan.uit@gmail.com>
2015-03-30 21:39:20 +0700

    commit some shit :panda_face:

--- Android/Android_AugmentedReality/AugmentedReality/app/src/main/java/app/com/augmentedreality/MainActivity.java
+++ Android/Android_AugmentedReality/AugmentedReality/app/src/main/java/app/com/augmentedreality/MainActivity.java
@@ -3,6 +3,7 @@ package app.com.augmentedreality;
 import android.app.Activity;
 import android.hardware.Camera;
 import android.hardware.Camera.Size;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,12 +14,16 @@ import android.view.SubMenu;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import app.com.augmentedreality.core.AugmentedCamera;
@@ -101,11 +106,37 @@ public class MainActivity extends Activity implements
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return null;
+        return inputFrame.rgba();
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
+        Log.i(TAG,"onTouch event");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String currentDateandTime = sdf.format(new Date());
+        String fileName = Environment.getExternalStorageDirectory().getPath() +
+                "/sample_picture_" + currentDateandTime + ".png";
+        mOpenCvCameraView.takePicture(fileName);
+        Toast.makeText(this, fileName + " saved", Toast.LENGTH_SHORT).show();
         return false;
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
     }
 }
