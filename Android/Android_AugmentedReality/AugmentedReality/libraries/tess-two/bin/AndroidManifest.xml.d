c3bf2561d7d60ebfcbd07645a992ca51b9e1df29
Đỗ Minh Quân <dominhquan.uit@gmail.com>
2016-02-10 16:23:37 +0700

    remove tmp file

--- Android/Android_AugmentedReality/AugmentedReality/libraries/tess-two/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\F.U.C.K\Documents\GitHub\Java\Android\Android_AugmentedReality\AugmentedReality\libraries\tess-two\bin\AndroidManifest.xml : \
-C:\Users\F.U.C.K\Documents\GitHub\Java\Android\Android_AugmentedReality\AugmentedReality\libraries\tess-two\AndroidManifest.xml \
--- Android/Android_AugmentedReality/AugmentedReality/libraries/tess-two/bin/tess-two.ap_.d
+++
@@ -1,2 +0,0 @@
-C:\Users\F.U.C.K\Documents\GitHub\Java\Android\Android_AugmentedReality\AugmentedReality\libraries\tess-two\bin\tess-two.ap_ \
- : C:\Users\F.U.C.K\Documents\GitHub\Java\Android\Android_AugmentedReality\AugmentedReality\libraries\tess-two\bin\AndroidManifest.xml \
